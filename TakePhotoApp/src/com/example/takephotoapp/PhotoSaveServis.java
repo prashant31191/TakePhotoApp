95fd6ca6b7e94347d5e17b98d01318517590690d
Satonina Anastasia <satonina.n@gmail.com>
2014-04-10 22:41:57 +0400

    .

---
+++ TakePhotoApp/src/com/example/takephotoapp/PhotoSaveServis.java
@@ -0,0 +1,87 @@
+package com.example.takephotoapp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class PhotoSaveServis extends Service {
+
+	private final IBinder DownloadServisBinder = new LocalBinder();
+	private String temporalFileName = "temporalPhoto.png";
+	private String temporalPhotoPath = null;
+
+	private File image;
+	private File temporalImage;
+
+	private File saveFileDirectory;
+	private File cacheDirectory;
+
+	private OutputStream output;
+	private InputStream input;
+
+	public class LocalBinder extends Binder {
+		PhotoSaveServis getService() {
+			return PhotoSaveServis.this;
+		}
+	}
+
+	public void savePhotoFile(String name) throws IOException {
+		if (temporalPhotoPath != null) {
+			saveFileDirectory = Environment
+					.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+			if (saveFileDirectory != null) {
+				image = new File(saveFileDirectory + File.separator + name
+						+ ".png");
+				try {
+					input = new FileInputStream(temporalImage);
+					output = new FileOutputStream(image);
+
+					byte[] bufphoto = new byte[1024];
+					int count;
+					while ((count = input.read(bufphoto)) > 0)
+						output.write(bufphoto, 0, count);
+
+				} catch (Exception e) {
+
+				} finally {
+					input.close();
+					output.close();
+				}
+			}
+
+		} else {
+			Toast.makeText(getApplicationContext(), "First make a photo",
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	public File createTemporalFile() throws IOException {
+		cacheDirectory = getExternalCacheDir();
+		if (cacheDirectory != null) {
+			temporalImage = new File(cacheDirectory + File.separator
+					+ temporalFileName);
+			temporalPhotoPath = temporalImage.getAbsolutePath();
+		}
+		return temporalImage;
+	}
+
+	public String getPhotoPath() {
+		return temporalPhotoPath;
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return DownloadServisBinder;
+	}
+
+}
---
+++ TakePhotoApp/src/com/example/takephotoapp/TakePhotoScreenActyvity.java
@@ -0,0 +1,160 @@
+package com.example.takephotoapp;
+
+
+import java.io.File;
+import java.io.IOException;
+
+
+
+import com.example.takephotoapp.PhotoSaveServis.LocalBinder;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class TakePhotoScreenActyvity extends Activity {
+	private Button takePhotoButton;
+	private ImageView photoView;
+	private PhotoSaveServis photoSaveService;
+	private Boolean bound = false;
+	private static final int REQUEST_TAKE_PHOTO = 1;
+	private final int DIALOG_SAVE = 1;
+	
+	private String photoPath;
+	private String fileName;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.a_take_photo_screen);
+		takePhotoButton = (Button) findViewById(R.id.takePhotoButton);
+		photoView = (ImageView) findViewById(R.id.photoView);
+
+		Intent intentPhotoServis = new Intent(this, PhotoSaveServis.class);
+		startService(intentPhotoServis);
+		bindService(intentPhotoServis, mConnection, Context.BIND_AUTO_CREATE);
+
+		if (photoPath != null) {
+			setPhotoImageView();
+		}
+
+		takePhotoButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				try {
+					takePictureIntent();
+				} catch (Exception e) {
+				}
+			}
+		});
+	}
+
+	private void takePictureIntent() throws IOException {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// Ensure that there's a camera activity to handle the intent
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+			File photoFile = photoSaveService.createTemporalFile();
+			if (photoFile != null) {
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+						Uri.fromFile(photoFile));
+				startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+			}
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		setPhotoImageView();
+	}
+
+	private void setPhotoImageView() {
+		photoPath = photoSaveService.getPhotoPath();
+		Bitmap bitmap = BitmapFactory.decodeFile(photoPath);
+		photoView.setImageBitmap(bitmap);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.take_photo_screen_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_save_photo) {
+			showDialog(DIALOG_SAVE);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	protected Dialog onCreateDialog(int id) {
+		if (id == DIALOG_SAVE) {
+			AlertDialog.Builder adb = new AlertDialog.Builder(this);
+
+			adb.setTitle(R.string.get_name_photo);
+			final EditText input = new EditText(this);
+			adb.setView(input);
+			adb.setPositiveButton("Œ ", new DialogInterface.OnClickListener() {
+
+				public void onClick(DialogInterface dialog, int whichButton) {
+					fileName = input.getText().toString();
+				     try {
+						photoSaveService.savePhotoFile(fileName);							
+					} catch ( IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+
+			return adb.create();
+		}
+		return super.onCreateDialog(id);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (bound) {
+			unbindService(mConnection);
+			bound = false;
+		}
+	};
+
+	private ServiceConnection mConnection = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			LocalBinder binder = (LocalBinder) service;
+
+			photoSaveService = binder.getService();
+			bound = true;
+
+			photoPath = photoSaveService.getPhotoPath();
+			if (photoPath != null) {
+				setPhotoImageView();
+			}
+		}
+
+		public void onServiceDisconnected(ComponentName arg0) {
+			bound = false;
+		}
+	};
+}
